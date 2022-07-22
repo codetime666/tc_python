e586b3caa13841e5430a920384222b407a256da7
价值0$的有趣灵魂 <37955079+codetime666@users.noreply.github.com>
2022-07-22 17:10:07 +0800

    Create 01.py

---
+++ AI小白/python 入门与实践/01.py
@@ -0,0 +1,130 @@
+###学习大纲
+
+## 1、变量、运算符与数据类型
+# 注释
+# 运算符
+# 变量和赋值
+# 数据类型与转换
+# print() 函数
+# 位运算
+# 原码、反码和补码
+# 按位非操作 ~
+# 按位与操作 &
+# 按位或操作 |
+# 按位异或操作 ^
+# 按位左移操作 <<
+# 按位右移操作 >>
+# 利用位运算实现快速计算
+# 利用位运算实现整数集合
+# 条件语句
+# if 语句
+# if - else 语句
+# if - elif - else 语句
+# assert 关键词
+# 循环语句
+# while 循环
+# while - else 循环
+# for 循环
+# for - else 循环
+# range() 函数
+# enumerate()函数
+# break 语句
+# continue 语句
+# pass 语句
+# 推导式
+# 异常处理
+# Python 标准异常总结s
+# Python 标准警告总结
+# try - except 语句
+# try - except - finally 语句
+# try - except - else 语句
+# raise语句
+
+
+#------------------------------------------------------------------------------
+## 一、变量、运算符与数据类型
+#1、注释
+#单行注释
+print("Hello world")
+#hello world
+#多行注释
+'''
+hello world
+'''
+"""
+hello world
+"""
+#2、运算符
+#算数运算符
+
+# +  -  *  /  //   %   **
+# 加 减 乘 除 整除 除余 幂
+#alt + shift + 下键复制当前行到下一行
+a = 1
+b = 2
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+#比较运算符
+#  >    >=      <     <=    ==    !=
+#大于 大于等于 小于 小于等于 等于 不等于
+
+print(a > b)
+print(a >= b)
+print(a < b)
+print(a <= b)
+print(a == b)
+print(a != b)
+# 逻辑运算符
+# and  or  not
+# 与   或   非
+c = print(1 > 1 and 1 < 2)
+print(1 > 0 or 1 < 2)
+d = False
+if not d:
+   print('hello')
+
+
+
+
+# 变量和赋值
+# 数据类型与转换
+# print() 函数
+# 位运算
+# 原码、反码和补码
+# 按位非操作 ~
+# 按位与操作 &
+# 按位或操作 |
+# 按位异或操作 ^
+# 按位左移操作 <<
+# 按位右移操作 >>
+# 利用位运算实现快速计算
+# 利用位运算实现整数集合
+# 条件语句
+# if 语句
+# if - else 语句
+# if - elif - else 语句
+# assert 关键词
+# 循环语句
+# while 循环
+# while - else 循环
+# for 循环
+# for - else 循环
+# range() 函数
+# enumerate()函数
+# break 语句
+# continue 语句
+# pass 语句
+# 推导式
+# 异常处理
+# Python 标准异常总结
+# Python 标准警告总结
+# try - except 语句
+# try - except - finally 语句
+# try - except - else 语句
+# raise语句
